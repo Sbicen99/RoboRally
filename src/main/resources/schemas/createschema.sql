70f11ee0ca69286718a56ac5830aa031c7e7e4cd
M-Najib Hebrawi <najiphebrawi@hotmail.com>
2021-04-15 00:57:53 +0200

    DataBaseConnection.

--- src/main/resources/schemas/createschema.sql
+++ src/main/resources/schemas/createschema.sql
@@ -30,3 +30,14 @@ CREATE TABLE IF NOT EXISTS Player (
 );;
 
 SET FOREIGN_KEY_CHECKS = 1;;
+
+create table if not exists CardField(
+gameID int not null,
+playerID tinyint not null,
+type tinyint not null,
+position tinyint not null,
+visible bit not null,
+command tinyint,
+primary key (gameID,playerID,type,position),
+foreign key (gameID)references Game(gameID),
+foreign key (gameID,playerID)references Player(gameID,playerID));
