3b85ee60d36ceff5e4873d60482ee98834900bdf
Thamara Chellakooty <thamaralinnea@gmail.com>
2021-05-11 21:59:35 +0200

    action added

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Gear.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Gear.java
@@ -4,6 +4,12 @@ import dk.dtu.compute.se.pisd.roborally.controller.GameController;
 
 public class Gear extends FieldAction {
 
+    public final String type;
+
+    public Gear(String type) {
+        this.type = type;
+    }
+
     /**
      * @param gameController the gameController of the respective game
      * @param space the space this action should be executed for
@@ -13,10 +19,15 @@ public class Gear extends FieldAction {
     @Override
     public boolean doAction(GameController gameController, Space space) {
         Player player = space.getPlayer();
-        if (player != null) {
+        if (player != null & type == "right") {
             player.setHeading(player.getHeading().next());
             return true;
-        } else {
+
+        } else if  (player != null & type == "left") {
+            player.setHeading(player.getHeading().prev());
+            return true;
+        }
+        else {
             return false;
         }
     }
